5b3d6f50b796d11c26160180235481c9e2d2cfd0
Chuan-Chin Lai <cclai@fb.com>
2023-08-09 13:38:08 -0700

    HP for hittable object

--- SeaGameJamTeam2/Assets/SeaGameJamTeam2/Scripts/Hittable.cs
+++ SeaGameJamTeam2/Assets/SeaGameJamTeam2/Scripts/Hittable.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Hittable : MonoBehaviour
 {
+    public int HP = 3;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,15 @@ public class Hittable : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Hit()
+    {
+        HP--;
+
+        if (HP == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
